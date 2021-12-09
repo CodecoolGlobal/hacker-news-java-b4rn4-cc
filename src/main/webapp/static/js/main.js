8d00cbaba1a3b2e05e75a98e2c930498b205294f
barna <barna.m42@gmail.com>
2021-12-09 11:33:18 +0100

    ADD: top news event listener and function

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -3,6 +3,16 @@ const prevButton = document.getElementById("prev");
 const contentDiv = document.getElementById("content")
 const nextButton = document.getElementById("next");
 
+topNewsElement.addEventListener("click", topNews);
+
+async function topNews() {
+    const news = await getTopNews();
+}
+
+async function getTopNews() {
+    return await apiGet("/top-news");
+}
+
 async function apiGet(url) {
     let response = await fetch(url, {
         method: "GET",
