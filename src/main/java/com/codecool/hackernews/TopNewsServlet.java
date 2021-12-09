29103c8415fe852526327a1a9abd0a17be5d2cce
barna <barna.m42@gmail.com>
2021-12-09 13:46:48 +0100

    REFACTOR: the backend api request in a static class

--- src/main/java/com/codecool/hackernews/TopNewsServlet.java
+++ src/main/java/com/codecool/hackernews/TopNewsServlet.java
@@ -5,9 +5,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.*;
 import java.util.Objects;
@@ -22,46 +20,9 @@ public class TopNewsServlet extends HttpServlet {
         String page;
         page = Objects.requireNonNullElse(requestPage, "1");
         URL url = new URL("https://api.hnpwa.com/v0/news/" + page +".json");
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer content = new StringBuffer();
-        while ((inputLine = in.readLine()) != null) {
-            content.append(inputLine);
-        }
-        in.close();
+        StringBuffer content = Util.getStringifiedJson(url);
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
         out.println(content);
-
-        /*StringBuffer buffer = new StringBuffer();
-        for (int i = 0; i < 10; i++) {
-            buffer.append("<div>");
-            buffer.append("<a href=\"/another?link_id=" + i + "\">");
-            buffer.append("Hello " + i + ". link:");
-            buffer.append("</a>");
-            buffer.append("</div>");
-        }
-
-        String linkId = request.getParameter("link_id");
-
-        out.println(
-                "<html>\n" +
-                        "  <head>" +
-                        "    <title>Another page</title>" +
-                        "    <script src='/static/js/main.js'></script>" +
-                        "  </head>\n" +
-                        "<body>\n" +
-                        "  <h1>Hello CodeCooler!</h1>" +
-                        (linkId == null ?
-                                "<h3>No link was pressed</h3>" :
-                                "<h3>Link " + linkId + " was pressed!</h3>") +
-                        "  <br/>" +
-                        "  <div>" + buffer.toString() + "</div>" +
-                        "</body>" +
-                        "</html>"
-        );*/
     }
 }
