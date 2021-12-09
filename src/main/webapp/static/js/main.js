e2647effe0cc3a664c30d877630aaf8c76c0e9e8
barna <barna.m42@gmail.com>
2021-12-09 11:31:44 +0100

    ADD: elements for event listeners and fetch

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -1,1 +1,13 @@
-alert("I'm from main.js");
+const topNewsElement = document.getElementById("top-news");
+const prevButton = document.getElementById("prev");
+const contentDiv = document.getElementById("content")
+const nextButton = document.getElementById("next");
+
+async function apiGet(url) {
+    let response = await fetch(url, {
+        method: "GET",
+    });
+    if (response.ok) {
+        return response.json();
+    }
+}
