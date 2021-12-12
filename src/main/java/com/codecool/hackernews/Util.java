79994a4fff618012e51cf5d91f0190e845113e21
barna <barna.m42@gmail.com>
2021-12-12 10:35:02 +0100

    ADD: serialization and deserialization to a static method

--- src/main/java/com/codecool/hackernews/Util.java
+++ src/main/java/com/codecool/hackernews/Util.java
@@ -1,10 +1,18 @@
 package com.codecool.hackernews;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Util {
     public static StringBuffer getStringifiedJson(URL url) throws IOException {
@@ -19,5 +27,19 @@ public class Util {
         }
         in.close();
         return content;
+    }
+
+    static void setupJson(HttpServletResponse response, URL url) throws IOException {
+        StringBuffer content = getStringifiedJson(url);
+
+        Type listType = new TypeToken<ArrayList<News>>(){}.getType();
+        List<News> newsList = new Gson().fromJson(String.valueOf(content), listType);
+
+        Gson gson = new Gson();
+        String fullJson = gson.toJson(newsList);
+
+        PrintWriter out = response.getWriter();
+
+        out.println(fullJson);
     }
 }
