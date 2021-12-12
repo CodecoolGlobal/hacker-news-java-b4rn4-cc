cceec9d6f2cdeb9ff4ee1caafddd3511602b4d3b
barna <barna.m42@gmail.com>
2021-12-12 10:24:04 +0100

    ADD: serializable class

---
+++ src/main/java/com/codecool/hackernews/News.java
@@ -0,0 +1,10 @@
+package com.codecool.hackernews;
+
+import java.io.Serializable;
+
+public class News implements Serializable {
+    private String title;
+    private int id;
+    private String user;
+    private String time_ago;
+}
