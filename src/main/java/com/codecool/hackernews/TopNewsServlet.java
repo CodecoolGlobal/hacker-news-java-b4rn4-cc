3ce4fb7a12d471a90b2cc24baf393e972c2b0ae3
barna <barna.m42@gmail.com>
2021-12-12 10:24:44 +0100

    ADD: serialization and deserialization for top news

--- src/main/java/com/codecool/hackernews/TopNewsServlet.java
+++ src/main/java/com/codecool/hackernews/TopNewsServlet.java
@@ -1,5 +1,8 @@
 package com.codecool.hackernews;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,7 +10,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.Type;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @WebServlet(name = "anotherServlet", urlPatterns = {"/top-news"}, loadOnStartup = 2)
@@ -21,8 +27,15 @@ public class TopNewsServlet extends HttpServlet {
         page = Objects.requireNonNullElse(requestPage, "1");
         URL url = new URL("https://api.hnpwa.com/v0/news/" + page +".json");
         StringBuffer content = Util.getStringifiedJson(url);
+        // serialization
+        Type listType = new TypeToken<ArrayList<News>>(){}.getType();
+        List<News> newsList = new Gson().fromJson(String.valueOf(content), listType);
+
+        Gson gson = new Gson();
+        String fullJson = gson.toJson(newsList);
+
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
-        out.println(content);
+        out.println(fullJson);
     }
 }
